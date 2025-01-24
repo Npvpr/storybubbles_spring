b55765b3f15fbc33aa79c9c51f430c040b6b2964
Npvpr <nlmnever2@gmail.com>
2025-01-24 21:54:04 +0000

    First Setup from Spring initializr

---
+++ src/main/java/storybubbles/storybubbles_spring/StorybubblesSpringApplication.java
@@ -0,0 +1,13 @@
+package storybubbles.storybubbles_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorybubblesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorybubblesSpringApplication.class, args);
+	}
+
+}
